241bfa6f82f38af8d31a5f289985875c8c21c841
udayasri-28 <udayasri682@gmail.com>
2026-04-07 15:20:49 +0530

    Added

---
+++ ProductAPI/ProductAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
